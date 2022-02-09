0da232dd9d5b65ab3d6007593b8f44c03ed8bcdc
Michael Gatto <mgatto@vinsuite.com>
2022-02-09 14:26:22 -0700

    Initial customation from template

---
+++ src/components/PastTrips/PastTripsList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PastTripsList: React.FC = () => {
+  return <h1>Your Past Trips</h1>;
+};
+
+export default PastTripsList;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
+import React from "react";
+import PastTripsList from "../components/PastTrips/PastTripsList";
+import { BrowserRouterProps } from "react-router-dom";
 
-const Home: React.FC = () => {
+const Home: React.FC<BrowserRouterProps> = (props) => {
+  // const
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <PastTripsList />
       </IonContent>
     </IonPage>
   );
